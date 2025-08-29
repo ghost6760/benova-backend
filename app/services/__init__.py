41769521e8c015696796a19fc96f997188409aec
ghost6760 <146762829+ghost6760@users.noreply.github.com>
2025-08-29 10:32:14 -0500

    Update __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,17 +1,21 @@
-"""Services package initialization"""
+"""Services package initialization - UPDATED with all services"""
 
 from .chatwoot_service import ChatwootService
 from .openai_service import OpenAIService, init_openai
-from .redis_service import get_redis_client, init_redis
-from .vectorstore_service import VectorstoreService
+from .redis_service import get_redis_client, init_redis, close_redis
+from .vectorstore_service import VectorstoreService, init_vectorstore
 from .multiagent_system import MultiAgentSystem
+from .multimedia_service import MultimediaService  # NOW IMPORTED
 
 __all__ = [
     'ChatwootService',
     'OpenAIService',
     'init_openai',
     'get_redis_client',
-    'init_redis',
+    'init_redis', 
+    'close_redis',
     'VectorstoreService',
-    'MultiAgentSystem'
+    'init_vectorstore',
+    'MultiAgentSystem',
+    'MultimediaService'  # NOW EXPORTED
 ]
