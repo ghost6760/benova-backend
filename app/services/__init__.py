1dfcd69b42582ca2815fe34afad8d29a447a0a47
ghost6760 <146762829+ghost6760@users.noreply.github.com>
2025-08-29 00:17:27 -0500

    Update __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,7 +1,7 @@
 """Services package initialization"""
 
 from .chatwoot_service import ChatwootService
-from .openai_service import OpenAIService
+from .openai_service import OpenAIService, init_openai
 from .redis_service import get_redis_client, init_redis
 from .vectorstore_service import VectorstoreService
 from .multiagent_system import MultiAgentSystem
@@ -9,6 +9,7 @@ from .multiagent_system import MultiAgentSystem
 __all__ = [
     'ChatwootService',
     'OpenAIService',
+    'init_openai',
     'get_redis_client',
     'init_redis',
     'VectorstoreService',
